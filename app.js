4e9b0d57c4c4359bba51ddc10b9fc4c81f26e51f
Ahmed Karasneh <karasneh94@gmail.com>
2021-01-31 15:59:00 +0200

    added css file for index and shahid files

--- app.js
+++ app.js
@@ -5,28 +5,33 @@ var userName = prompt('Enter Your Name');
 alert('Hello ' + userName + ', We have a gift for you at the end of the page 🎁');
 
 
-
-var typeOfShow = prompt(userName+', Do you prefer Movies or Series?');
-
-while(typeOfShow !== 'movies' && typeOfShow !== 'series'){
-    typeOfShow = prompt(userName +', Trust me this will keep going forever if you dont answer with either movies or series')
+function showType(name) {
+    
+    var typeOfShow = prompt(name+', Do you prefer Movies or Series?');
+    
+    while(typeOfShow !== 'movies' && typeOfShow !== 'series'){
+        typeOfShow = prompt(name +', Trust me this will keep going forever if you dont answer with either movies or series')
+    }
+    
+    if (typeOfShow == 'movies' || typeOfShow == 'Movies')
+    {
+        document.write('<br><h2>'+name+ ', Here are the top 3 Movies of all time</h2><br>');
+        showList = '<ol><li><h3>The Shawshank Redemption</h3></li><li><h3>The Godfather</h3></li><li><h3>the Dark Knight</h3></li></ol>';
+    
+    }else if (typeOfShow == 'series' || typeOfShow == 'Series'){
+        document.write('<br><h2>'+name+ ', Here are the top 3 Series of all time</h2><br>');
+        showList = '<ol><li><h3>Friends</h3></li><li><h3>HIMYM</h3></li><li><h3>Vikings</h3></li></ol>';
+    
+    }else{
+        showList = '<h1>GO WATCH ANIME</h1>'
+    }
 }
 
-if (typeOfShow == 'movies' || typeOfShow == 'Movies')
-{
-    document.write('<br><h2>'+userName+ ', Here are the top 3 Movies of all time</h2><br>');
-    showList = '<ol><li><h3>The Shawshank Redemption</h3></li><li><h3>The Godfather</h3></li><li><h3>the Dark Knight</h3></li></ol>';
-
-}else if (typeOfShow == 'series' || typeOfShow == 'Series'){
-    document.write('<br><h2>'+userName+ ', Here are the top 3 Series of all time</h2><br>');
-    showList = '<ol><li><h3>Friends</h3></li><li><h3>HIMYM</h3></li><li><h3>Vikings</h3></li></ol>';
-
-}else{
-    showList = '<h1>GO WATCH ANIME</h1>'
-}
+showType(userName);
 
 
 var feedback = prompt(userName+', How many random movie posters you wanna see?');
+
 
 var userColor = confirm('One last thing '+userName+' , Would you like to see a light background of this page?');
 if (userColor == true) {
